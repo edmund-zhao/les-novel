aa3cea22d45884cc83624a2e97aab4b3ccd24b0f
ED_Z <53864204+edmund-zhao@users.noreply.github.com>
2021-04-27 09:03:42 +0800

    Add files via upload

--- ddxstxt8/getChapter.py
+++ ddxstxt8/getChapter.py
@@ -1,37 +1,33 @@
 import grequests
-import requests
 from lxml import etree
-from requests.adapters import HTTPAdapter
 
+import re
 headers = {
 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
 }
-def get(bookUrl):
+def get(bookUrl, session):
     chapterUrl = []
-    s = requests.Session()
-    s.mount('http://', HTTPAdapter(max_retries=3))
-    s.mount('https://', HTTPAdapter(max_retries=3))
-    respond = s.get(bookUrl,headers = headers, timeout = 7)
-    respond.encoding = 'gbk'
-    tree = etree.HTML(respond.text)
     try :
+        respond = session.get(bookUrl, headers=headers, timeout=7)
+        respond.encoding = 'gbk'
+        tree = etree.HTML(respond.text)
         bookName = tree.xpath('//*[@id="info"]/h1')[0].text
         authorName = tree.xpath('//*[@id="info"]/p[1]')[0].text.split('：')[-1]
         coverUrl = 'https://www.ddxstxt8.com' + tree.xpath('//div[@id="fmimg"]/img')[0].get('src')
+        bookName = illegalStrip(bookName).strip()
+        authorName = illegalStrip(authorName).strip()
     except:
         print('网络错误')
         return -1, -1 , -1 ,-1
     for i in tree.xpath('//*[@id="list"]/dl/dd[*]/a/@href'):
         url = respond.url + i
-        chapterUrl.append(grequests.get(url ,session = s, headers = headers))
+        chapterUrl.append(grequests.get(url, session = session, headers = headers, timeout = 7))
     return chapterUrl , coverUrl, bookName, authorName
-t = get('https://www.ddxstxt8.com/0_736/')
 
-for respond in grequests.map(t[0],gtimeout=10):
-    respond.encoding = 'gbk'
-    tree = etree.HTML(respond.text)
-    h1 = tree.xpath('//div[@class="bookname"]/h1')[0].text
-    content = tree.xpath('//*[@id="content"]/text()[*]')
-    print(h1)
-    for i in content:
-        print(i)
+def illegalStrip(path):
+    """
+    :param path: 需要清洗的文件夹名字
+    :return: 清洗掉Windows系统非法文件夹名字的字符串
+    """
+    path = re.sub(r'[？?\*|“《》<>:/]', '', str(path))
+    return path
